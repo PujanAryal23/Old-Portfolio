ef271dd4dfd29d985228a0650b6554a2a60df345
Pujan Aryal <pry@logpoint.com>
2023-04-07 12:53:06 +0545

    add loading feature

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,6 +1,10 @@
+const loadingOverlay = document.querySelector('#loading-overlay');
+
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // prevent the default form submit behavior
-  
+    event.preventDefault(); // prevent the default form submit behavior\
+    debugger;
+    loadingOverlay.classList.remove('hidden');
+
     // Get the form fields
     var name = document.getElementById("name").value.trim();
     var email = document.getElementById("email").value.trim();
@@ -18,6 +22,8 @@ document.getElementById("contact-form").addEventListener("submit", function(even
     emailjs.send("service_j4pbqni", "template_pa70ni4", template_params)
       .then(function(response) {
         console.log("Email sent!", response.status, response.text);
+        document.getElementById('loading-icon').style.display = 'none';
+        loadingOverlay.classList.add('hidden');
         var successModal = new bootstrap.Modal(document.getElementById("successModal"));
         successModal.show();
         document.getElementById("contact-form").reset();
